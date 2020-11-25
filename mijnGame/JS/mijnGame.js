c129d555215c46b16b27dbc5774e3219ccf09731
TjalleDurkvde <tjalledurk@gmail.com>
2020-11-25 13:50:09 +0000

    bets gveel

--- mijnGame/JS/mijnGame.js
+++ mijnGame/JS/mijnGame.js
@@ -1,4 +1,6 @@
+//var speler = {
 
+//}
 var xJager = 0;
 var yJager = 400;
 
@@ -13,59 +15,52 @@ var white = 'lightgray';
 var achtergrond = 'white';
 var doodnummer = 0;
 
-var plateaus = new Array (plateau(0,500,500,500),plateau(800,500,1120,500));
+//var plateaus = new Array(plateau(0, 500, 500, 500), plateau(800, 500, 1120, 500));
 
 
 class Jager {
-  vallen(){
-    yJager -= 12;
+  vallen() {
+    yJager -= 20;
   }
 
 }
 class plateau {
-  constructor(x,y,w,h){
-    this.x =x;
-    this.y =y;
-    this.w =w;
-    this.h =h;
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
   }
 }
 
 
 
 
-
-  function setup() {
-    canvas = createCanvas(1920,1080);
-    textFont("Verdana");
-    textSize(140);
-    noStroke();
-    frameRate(50);
-  }
+function setup() {
+  canvas = createCanvas(1920, 1080);
+  textFont("Verdana");
+  textSize(140);
+  noStroke();
+  frameRate(50);
+}
 
 
 //Onoff gedoe //
 function draw() {
-  
-  background(achtergrond);
-  if (keyIsDown(32) && onoff == 1 ){
-    onoff = 2;
-    white = 'white';
-    black = 'darkgray';
-    achtergrond = 'black';
-    setInterval(100);
-  }
-  else if (keyIsDown(32) && onoff == 2){
-    onoff = 1;
-    white = 'lightgray';
+
+ background(achtergrond);
+
+  if (onoff == 1) {
     black = 'black';
+    white = 'lightgray';
     achtergrond = 'white';
-    setInterval(100);
-
-   
   }
 
-  
+  if (onoff == 2) {
+    black = 'darkgray';
+    white = 'white';
+    achtergrond = 'black';    
+  }
   //Movements//
   if (keyIsDown(LEFT_ARROW)) {
     xJager -= 15;
@@ -76,30 +71,30 @@ function draw() {
   if (keyIsDown(UP_ARROW)) {
     yJager -= 12;
     //for(var n = 1; n < 4 ; n++){
-      //yJager -= n*10
+    //yJager -= n*10
   }
 
   else {
-   yJager += zwaartekracht;
+    yJager += zwaartekracht;
   }
 
-  //if (xJager >= plateau.x && xJager <= plateau.x + plateau.w && yJager >= plateau.y - 100){
-    //yJager = constrain(yJager,0,400);
+  if (xJager >= plateau.x && xJager <= plateau.x + plateau.w && yJager >= plateau.y - 100){
+  yJager = constrain(yJager,0,400);
 
 
-//}
+  }
 
- //plateaus//
+  //plateaus//
   if (xJager <= 500 && onoff == 1) {
-    yJager = constrain(yJager,0,400);
-    
+    yJager = constrain(yJager, 0, 400);
+
   }
-  if (xJager >=700 && onoff == 2){
-    yJager = constrain(yJager,0,400);
+  if (xJager >= 700 && onoff == 2) {
+    yJager = constrain(yJager, 0, 400);
   }
   //if (xJager >= 500 && xJager <= 700 && yJager <=400){
     //xJager = constrain(xJager,500,0);
-    //xJager = constrain(xJager,700,0);
+   // xJager = constrain(xJager,700,0);
   //}
 
   xJager = constrain(xJager, 0, width - 100);
@@ -111,29 +106,29 @@ function draw() {
     eindScherm();
   }
 
-  if (yJager >= height - 120){
+  if (yJager >= height - 100) {
     dood();
   }
 
   else {
     fill('darkkhaki');
   }
-  rect(xSter,ySter,75, 50);
+  rect(xSter, ySter, 75, 50);
   fill('moccasin');
-  rect(xJager,yJager, 100, 100);
+  rect(xJager, yJager, 100, 100);
   fill(black);
-  rect(0,500,500,500);
+  rect(0, 500, 500, 500);
   fill(white)
-  rect(800,500,1120,500)
+  rect(800, 500, 1120, 500)
 }
 fill('blue');
-text(doodnummer,75,250);
+text(doodnummer, 75, 250);
 
 //Doodgaan//
 function eindScherm() {
   background('white');
   fill('black');
-  text("dood", 75, 250);
+  text("Next", 75, 250);
   noLoop();
 }
 function dood() {
@@ -141,12 +136,22 @@ function dood() {
   yJager = 400;
   onoff = 1;
   doodnummer += 1;
-  text(doodnummer,75,250);
+  text(doodnummer, 75, 250);
   white = 'lightgray';
   black = 'black';
   achtergrond = 'white';
 }
+function keyPressed() {
+  if (keyCode === 'a'){
+    if (onoff === 1) {
+      onoff = 2
+    }
+  } 
+    else {
+      onoff = 1
+    }
+}
 
 
-    
-  
+
+
